4929638fad61a7d60bb9f7d1fbf48fd23d696273
Toine Haaijer <haaijer.toine@gmail.com>
2017-02-14 10:47:38 +0100

    CSS voor top-links en aanpassingen in config en js/css paths

--- config.php
+++ config.php
@@ -12,7 +12,7 @@ define("DIR_CLASSES", DIR_BASE .  '/classes');
 define("DIR_CONFIG", DIR_BASE .  '/config');
 define("DIR_SKIN",  DIR_BASE .  '/skin');
 define("DIR_PAGE",  DIR_BASE .  '/page');
-define("DIR_BOOTSTRAP", DIR_SKIN . '/bootstrap');
+define("DIR_MATERIALIZE", DIR_SKIN . '/materialize');
 define("DIR_CSS", DIR_SKIN . '/css');
 define("DIR_JS", DIR_SKIN . '/js');
 define("HEAD", DIR_PAGE . '/head.phtml');
--- config/config.inc.php
+++ config/config.inc.php
@@ -11,15 +11,15 @@ define("DIR_INCLUDES", ROOT . 'includes/');
 define("DIR_CLASSES", ROOT . 'classes/');
 define("DIR_SKIN", ROOT . 'skin/');
 define("DIR_PAGE", ROOT . 'page/');
-define("DIR_CONTENT", DIR_PAGE . '/content');
-define("DIR_MATERIALIZE", DIR_SKIN . '/materialize');
-define("DIR_CSS", DIR_SKIN . '/css');
-define("DIR_JS", DIR_SKIN . '/js');
-define("HEAD", DIR_PAGE . '/head.inc.phtml');
-define("HEADER", DIR_PAGE . '/header.inc.phtml');
-define("TOPLINKS", DIR_PAGE . '/toplinks.inc.phtml');
-define("NAV", DIR_PAGE . '/nav.inc.phtml');
-define("FOOTER", DIR_PAGE . '/footer.inc.phtml');
-define("SCRIPTS", DIR_PAGE . '/scripts.inc.phtml');
+define("DIR_CONTENT", DIR_PAGE . 'content/');
+define("DIR_MATERIALIZE", DIR_SKIN . 'materialize/');
+define("DIR_CSS", DIR_SKIN . 'css/');
+define("DIR_JS", DIR_SKIN . 'js/');
+define("HEAD", DIR_PAGE . 'head.inc.phtml');
+define("HEADER", DIR_PAGE . 'header.inc.phtml');
+define("TOPLINKS", DIR_PAGE . 'toplinks.inc.phtml');
+define("NAV", DIR_PAGE . 'nav.inc.phtml');
+define("FOOTER", DIR_PAGE . 'footer.inc.phtml');
+define("SCRIPTS", DIR_PAGE . 'scripts.inc.phtml');
 
 ?>
